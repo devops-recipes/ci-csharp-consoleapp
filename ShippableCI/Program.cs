5bb44f49b74d47df90b822ee3c1dffb5058ea512
ambarish2012 <ambarish@shippable.com>
2017-12-05 13:19:25 -0800

    Initial commit

---
+++ ShippableCI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace ShippableCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JArray array = new JArray();
+            array.Add("Manual text");
+            array.Add(new DateTime(2000, 5, 23));
+
+            JObject o = new JObject();
+            o["MyArray"] = array;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("JSON string: " + o.ToString());
+        }
+    }
+}
